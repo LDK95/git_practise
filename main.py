c3e167a55e1e94d40dda07a3cacb1fb595b4cff5
Lee Dong Keun <gkh02332@naver.com>
2023-05-02 02:28:03 +0900

    feat : Define Print_hello() (Trailling comma test)
    Print hello to all names in names.

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 # Test python env
-print('hello')
+def print_hello():
+    animals = ['dog', 'cat', 'hamster']
+    foods = [
+        'spagetti',
+        'pizza'
+    ] # w/o trailing comma
+    names = [
+        'John',
+        'Jane',
+        'Gil-dong',
+    ] # w/ trailing comma
+    for f_name in names:
+        print(f'hello, {f_name}')
+
+
+if ___name__== '__main__':
+    print_hello()
